42e33c012fe5b390ed6760afbdc9ad9287cdbdd5
WilliamAWSOFTWARE <45134675+WilliamAWSOFTWARE@users.noreply.github.com>
2019-06-25 16:50:07 -0400

    Se arreglaron la gran parte de los errores
    Todas las pruebas de pycharm funcionales

--- EJERCICIO 1.6/EJERCICIO 1.6/main.c
+++ EJERCICIO 1.6/EJERCICIO 1.6/main.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Las entradas de este ejercicio tenian que ser la velocidad maxima y el tiempo de respuesta -10
-int v,d;
+// Las entradas de este ejercicio tenian que ser la velocidad maxima y el tiempo de respuesta -10(RESUELTO)
+float v,da, t;
 int main()
 {
-    printf("Ingrese la velocidad en m/s:");
+    //La pido en M/S para evitar la conversion de Km/h a m/s
+    printf("Ingrese la velocidad maxima de la calle(m/s):");
     scanf("%d", &v);
-    printf("Ingrese la distancia en metros:");
-    scanf("%d", &d);
-    printf("El tiempo es de: %.2f", (2*v)/d);
+    printf("Ingrese la velocidad de frenado(m/s):");
+    scanf("%d", &da);
+    printf("Ingrese tiempo de respuesta del conductor(segundos):");
+    scanf("%d", &t);
+    printf("El tiempo del semafaro es de: %.2f segundos\n", ((v/da)+t));
 
 }
